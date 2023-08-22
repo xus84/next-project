cd726b3dfd2b2d6d700aab5d8e55028620ef2561
xus84 <g_garsan@hotmail.com>
2023-08-22 13:43:00 +0200

    routes practized

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (<>
+        <nav>
+            <ul>
+
+                <p>1</p>
+
+
+                <p>2</p>
+
+            </ul>
+        </nav>
+    </>)
+}
---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function homepage(){
+    return <h1>Hello world!!</h1>
+}
