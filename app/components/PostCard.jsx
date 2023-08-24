f5751c19455a50725bbfa9a56434729e0b89e31d
xus84 <g_garsan@hotmail.com>
2023-08-24 19:50:22 +0200

    params added

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -1,11 +1,20 @@
 'use client';
+import Link from "next/link";
 
 function PostCard({ post }) {
     return (
         <div>
-            <h3>{post.id}. {post.title}</h3>
-            <p>{post.body}</p>
-            <button onClick={() => { alert('render') }}>click</button>
+         <Link href={`/posts/${post.id}`}>
+            <h3>
+                {post.id}. {post.title}
+            </h3>
+         </Link>
+         <p>{post.body}</p>
+         <button onClick={() => {
+            alert("click")
+         }}>
+            click
+         </button>
         </div>
     )
 }
